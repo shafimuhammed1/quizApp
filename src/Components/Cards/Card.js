5ad85a2fc5501bccee006b6949d7590852c9c3c0
Muhammed Shafi <mohdshafi13112000@gmail.com>
2023-09-12 14:49:15 +0530

    Added a component called card and styled it

--- src/Components/Cards/Card.js
+++ src/Components/Cards/Card.js
@@ -1,19 +1,24 @@
 import React from 'react'
+import './Card.css'
 
 function Card() {
   return (
     <div className='Card'>
-<div class="question-card">
- 
- 
-    <h4><b>John Doe</b></h4>
-   
-  </div>
-</div>
-
-      
-    
-  )
+      <div className='Card-container'>
+      <div class="question-card">
+       <h4 className='ans'><b>John Doe</b></h4>
+      </div>
+     <div class="question-card1">
+      <h4 className='ans'><b>John Doe</b></h4>
+     </div>
+     <div class="question-card2">
+      <h4 className='ans'><b>John Doe</b></h4>
+     </div>
+     <div class="question-card3">
+      <h4 className='ans'><b>John Doe</b></h4>
+     </div>
+     </div>
+    </div>
+ )
 }
-
 export default Card
