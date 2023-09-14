aaeed84f50df8e02698ddbd15defc7747edb013f
Muhammed Shafi <mohdshafi13112000@gmail.com>
2023-09-14 13:12:10 +0530

    created a json file and add question options and value

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import NavBar from "./Components/NavBar/NavBar";
 import Container from "./Components/Container/Container";
 import './App.css'
 import Card from "./Components/Cards/Card";
+import FootBar from "./Components/FootBar/FootBar";
 
 
 function App() {
+
+
+  
   return (
     <div className="App">
      <NavBar/>
      <Container/>
      <Card/>
+     <FootBar/>
     </div>
   );
 }
---
+++ src/Components/FootBar/FootBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './FootBar.css'
+
+function FootBar() {
+  return (
+    <div className='footbar'>
+      <button className='button3'>!!</button>
+      
+      
+
+    </div>
+  )
+}
+
+export default FootBar
