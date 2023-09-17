5eaeb7d264afb0171d8f3aa1f3f91530aaf90e72
Sahilm416 <114013781+Sahilm416@users.noreply.github.com>
2023-09-17 13:40:34 +0530

    fixed lil things

--- src/Components/Cards/Card.js
+++ src/Components/Cards/Card.js
@@ -1,18 +1,48 @@
-import React from 'react'
-import './Card.css'
+import React from "react";
+import "./Card.css";
+import "./Container.css";
+import { useState } from "react";
+function Card({ data, handleCardClick,count }) {
+  
 
-
-function Card({ option, handleCardClick }) {
   return (
-    <div className='Card'>
-      <div className='Card-container'  >
-        {option.map((option)=>(
-        <div className='question-card' onClick={() => handleCardClick(option)}>
-          <h4 className='ans'><b>{option.text}</b></h4>
-        </div>
-      ))}
-       </div>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "40px",
+        flexDirection: "column",
+      }}
+    >
+      <h1>{data[count].question}</h1>
+      {data[count].options.map((opt) => {
+        return (
+          <>
+            <div
+              style={{
+                color: "white",
+                cursor: "pointer",
+                padding: "5px",
+                backgroundColor: "black",
+                borderRadius: "5px",
+                width: "200px",
+                textAlign:"center"
+              }}
+              onClick={() => {
+                handleCardClick(opt);
+                
+              }}
+            >
+              <h3>{opt.text}</h3>
+            </div>
+          </>
+        );
+      })}
+     
     </div>
   );
 }
-export default Card
+export default Card;
